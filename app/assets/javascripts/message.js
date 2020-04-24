a96b8c4f39866d0214ec215362cefbe891636b50
atsukofu <atsukofu0527@gmail.com>
2020-04-24 21:10:02 +0900

    メッセージ自動更新機能を実装しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,3 +64,71 @@ $(function() {
     });
   })
 });
+
+$(function(){
+  function buildHTML(message) {
+    if ( message.image ) {
+      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__create-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__message-text">
+                        ${message.content}
+                      </p>
+                    </div>
+                    <img src=${message.image} >
+                  </div>`
+        return html;
+      } else {
+      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__create-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__message-text">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+        return html;
+      };
+    }
+ 
+  var reloadMessages = function() {
+    var last_message_id = $('.wrapper__chat-main__message-list__message-box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i,message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.wrapper__chat-main__message-list').append(insertHTML);
+        $('.wrapper__chat-main__message-list').animate({ scrollTop: $('.wrapper__chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
+});
+
