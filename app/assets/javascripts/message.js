082f5297d811470ab7b35c9b0d53631e73b3cfc2
atsukofu <atsukofu0527@gmail.com>
2020-04-24 23:01:59 +0900

    余分なメソッドを削除しました
    関数buildHTMLを2つ記入していたため、片方に統一しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,9 +2,9 @@ $(function() {
 
   function buildHTML(message) {
     if (message.image) {
-      var html = `<div class="wrapper__chat-main__message-list__message-box">` +
+      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>` +
                    `<div class="upper-message">` +
-                     `<p class="upper-message__user-name">` +
+                     `<p class="upper-message__created-date">` +
                        message.user_name + 
                      `</p>` +
                      `<p class="upper-message__create-date">` +
@@ -20,12 +20,12 @@ $(function() {
                  `</div>`
       return html;
     } else {
-      var html = `<div class="wrapper__chat-main__message-list__message-box">` +
+      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>` +
                    `<div class="upper-message">` +
                      `<p class="upper-message__user-name">` +
                        message.user_name + 
                      `</p>` +
-                     `<p class="upper-message__create-date">` +
+                     `<p class="upper-message__created-date">` +
                        message.created_at +
                      `</p>` +
                    `</div>` +
@@ -38,7 +38,7 @@ $(function() {
       return html;
     };
   }
-
+      
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -63,48 +63,7 @@ $(function() {
       $('.wrapper__chat-main__message-form__input-box__submit-btn').prop('disabled', false);
     });
   })
-});
 
-$(function(){
-  function buildHTML(message) {
-    if ( message.image ) {
-      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__create-date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__message-text">
-                        ${message.content}
-                      </p>
-                    </div>
-                    <img src=${message.image} >
-                  </div>`
-        return html;
-      } else {
-      var html = `<div class="wrapper__chat-main__message-list__message-box" data-message-id=${message.id}>
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__create-date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__message-text">
-                        ${message.content}
-                      </p>
-                    </div>
-                  </div>`
-        return html;
-      };
-    }
- 
   var reloadMessages = function() {
     var last_message_id = $('.wrapper__chat-main__message-list__message-box:last').data("message-id");
     $.ajax({
@@ -128,7 +87,10 @@ $(function(){
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 7000);
+    setInterval(reloadMessages, 7000);
   }
 });
+      
 
+
+  
